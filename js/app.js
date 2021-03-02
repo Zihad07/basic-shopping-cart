270264260735578519adafe03ec025a16d1d7613
Zihad <zihadnayem8@gmail.com>
2021-03-02 12:11:20 +0600

    added different type of vairable in app.js file.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-console.log('connected');
+console.log("connected");;
+
+// variable declare section
+const totalPrice = document.getElementById("total");
+const tax = document.getElementById("tax");
+const subTotal = document.getElementById("subtotal");
+
+
+const phoneDecrementBtn = document.getElementById("phone-price-decrement");
+const phoneIncrementBtn = document.getElementById("phone-price-increment");
+const phonePrice = document.getElementById("phone-price");
+const phoneQuantity = document.getElementById("phone-quantity");
+
+const caseDecrementBtn = document.getElementById("case-price-decrement");
+const caseIncrementBtn = document.getElementById("case-price-increment");
+const casePrice = document.getElementById("case-price");
+const caseQuantity = document.getElementById("case-quantity");
+
+console.log(totalPrice.innerText);
+console.log(phoneQuantity.value);
+console.log(caseQuantity.value);
+
+
+// increment and decrement handler
